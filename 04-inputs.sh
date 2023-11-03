18a963dffb29a6a1f358e7c08b2695f891e1a39e
EC2 Default User <ec2-user@ip-172-31-7-219.ec2.internal>
2023-11-03 06:25:43 +0000

    addition

--- 04-inputs.sh
+++ 04-inputs.sh
@@ -1,2 +1,11 @@
-echo $1
-echo $2
+# 0-n , $*/$@, $#
+
+echo First Argument $1 # First Argument
+echo Second Argument $2 # Second Argument
+
+echo Script Name $0 # Script Name
+
+
+echo  All Arguments = $* # All Arguments
+echo All Arguments = $@ #All Arguments
+echo Number of Arguments = $# # Number of Arguments
