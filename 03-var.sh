138ab42395541d75a09ae7c03753691062dc5c2b
EC2 Default User <ec2-user@ip-172-31-7-219.ec2.internal>
2023-11-03 06:04:24 +0000

    modified2

--- 03-var.sh
+++ 03-var.sh
@@ -5,3 +5,7 @@ echo "Hello, Good Morning, Today's Date is ${Date}"
 #Date=$(date +'%Y-%m-%d')
 Date=$(date)
 echo "Hello, Good Morning, Today's Date is ${Date}"
+
+# Arithmetic substitution,
+ADD=$((2+3))
+echo Addition of 2+3 = ${ADD}
