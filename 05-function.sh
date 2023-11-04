7e22f793502470a58b791f4cbfa046f3e7bbaafa
EC2 Default User <ec2-user@ip-172-31-7-219.ec2.internal>
2023-11-04 06:00:09 +0000

    2

--- 05-function.sh
+++ 05-function.sh
@@ -1,8 +1,14 @@
 ## Declare a function
 xyz() {
     echo Hello from Function
+    echo First Argument - $1
+    echo Second Argument -- $2
+    echo All Arguments - $*
+    echo No of Arguments - $#
 }
 
+## Main Program
 ## Call a function
-xyz
-    
+xyz 123 456
+
+
